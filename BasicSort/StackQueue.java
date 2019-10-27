91139fe5c08b7e1a04cb50c2c7352789c9ef66a3
qfsong <qfsong108@gmail.com>
2019-10-27 20:06:06 +0800

    add stack queue

---
+++ BasicSort/StackQueue.java
@@ -0,0 +1,36 @@
+public class StackQueue{
+	public static void main(String[] args) {
+		
+	}
+
+
+	public static class Stack<T>{
+
+		public T[] values;
+
+		public Stack(){
+
+		}
+
+		public void push(T value){
+			if () {
+				
+			}
+		}
+
+		public T pop(){
+
+		}
+
+		public boolean isEmpty(){
+
+			return false;
+		}
+
+	}
+
+
+	public static class Queue{
+
+	}
+}
