44926e0c6f60feb7c15b2238860e3caf3a2ef2fa
qfsong <qfsong108@gmail.com>
2019-11-05 22:32:17 +0800

    add depth search

--- BasicSort/Tree.java
+++ BasicSort/Tree.java
@@ -3,8 +3,13 @@ import java.util.Stack;
 
 public class Tree{
 	public static void main(String[] args) {
-		
+		testDepth();
 	}
+
+	private static void testDepth(){
+		depthFirstSearch(TreeNode.buildTest());
+	}
+
 
 	public static class TreeNode{
 		public TreeNode left;
@@ -16,9 +21,26 @@ public class Tree{
 			this.value = value;
 		}
 
+		/**
+		*    0
+		*   / \
+		*	1  2
+		**/
 		public static TreeNode buildTest(){
-
-			return null;
+			TreeNode root = new TreeNode(0);
+			TreeNode node = new TreeNode(1);
+			TreeNode node1 = new TreeNode(2);
+			TreeNode node2 = new TreeNode(3);
+			TreeNode node3 = new TreeNode(4);
+			TreeNode node4 = new TreeNode(5);
+			TreeNode node5 = new TreeNode(6);
+			root.left = node;
+			root.right = node1;
+			node.left = node2;
+			node.right = node3;
+			node1.left = node4;
+			node1.right = node5;
+			return root;
 		}
 
 		public static TreeNode build(int[] data){
@@ -36,8 +58,8 @@ public class Tree{
 				TreeNode node = stack.pop();
 				if (node != null) {
 					System.out.println(" node: " + node.value);
-					stack.push(node.left);
 					stack.push(node.right);
+					stack.push(node.left);
 				}
 			}
 		}
